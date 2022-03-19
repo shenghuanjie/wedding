9701865a052d5041e0d259f085e54513081c3a2a
Huanjie Sheng <drhjsheng@gmail.com>
2022-03-19 12:42:08 -0700

    update

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -5,7 +5,7 @@
  * License : MIT
  * Version : 1.3.2
  * Author : Vincent Loy <vincent.loy1@gmail.com>
- * Contributors : 
+ * Contributors :
  *  - Justin Beasley <JustinB@harvest.org>
  *  - Nathan Smith <NathanS@harvest.org>
  */
@@ -115,10 +115,10 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
-                hours: 0,
+                year: 2022,
+                month: 10,
+                day: 1,
+                hours: 12,
                 minutes: 0,
                 seconds: 0,
                 words: {
